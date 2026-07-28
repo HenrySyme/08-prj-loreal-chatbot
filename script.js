6360b4dfaed84119989e162aabd40722f8a7ad52
Henry Syme <henrysyme@gmail.com>
2026-07-28 21:19:40 +0000

    made a functional ai backend and branding frontend

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ const chatForm = document.getElementById("chatForm");
 const userInput = document.getElementById("userInput");
 const chatWindow = document.getElementById("chatWindow");
 
+const workerUrl = 'https://webdevelopmentforeveryonebot.henrysyme.workers.dev';
+
+let messages = [{role: 'system', content: 'You are a big L’Oréal enthusist. You only answers queries about L’Oréal products and routines. Anything else you politely decline from answering.'}]
+
 // Set initial message
-chatWindow.textContent = "👋 Hello! How can I help you today?";
+chatWindow.textContent = "Hello, what do you need?";
 
 /* Handle form submit */
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  // When using Cloudflare, you'll need to POST a `messages` array in the body,
-  // and handle the response using: data.choices[0].message.content
-
-  // Show message
-  chatWindow.innerHTML = "Connect to the OpenAI API for a response!";
+  messages.push({ role: 'user', content: `${userInput.value}`});
+  talkToAI();
 });
+
+
+async function talkToAI(){
+  const response = await fetch(workerUrl, {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      model: 'gpt-4o',
+      messages: messages})
+  });
+  chatWindow.innerHTML = "loading...";
+  const result = await response.json();
+  chatWindow.innerHTML = result.choices[0].message.content;
+}
